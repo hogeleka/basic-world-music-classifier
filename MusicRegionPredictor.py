54d52c24826b83e0e7bccf78856b1a8ea415c3e8
hogeleka <hogeleka@macalester.edu>
2020-05-09 20:56:27 -0500

    more fixes with machine learning model

--- MusicRegionPredictor.py
+++ MusicRegionPredictor.py
@@ -25,48 +25,14 @@ import keras
 import warnings
 warnings.filterwarnings('ignore')
 
-# class MusicRegionPredictor():
-#
-#     def __init__(self):
-#
-#         self.model = load_model()
-#         pass
-#
-#     def predictRegion(self, audioFilePath):
-#         modelFileName = "musicGenreModel_1000New.h5"
-#         y, sr = librosa.load(audioFilePath, mono=True, duration=30)
-#         dataRow = []
-#         chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-#         dataRow.append(np.mean(chroma_stft))
-#         rmse = librosa.feature.rms(y=y)
-#         dataRow.append(np.mean(rmse))
-#         spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-#         dataRow.append(np.mean(spec_cent))
-#         spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-#         dataRow.append(np.mean(spec_bw))
-#         rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-#         dataRow.append(np.mean(rolloff))
-#         zcr = librosa.feature.zero_crossing_rate(y)
-#         dataRow.append(np.mean(zcr))
-#         mfcc = librosa.feature.mfcc(y=y, sr=sr)
-#         for e in mfcc:
-#             dataRow.append(np.mean(e))
-#         dataRowNumpyArray = np.array(dataRow)
-#         model = load_model(modelFileName)
-#         model.summary()
-#         predicted = model.predict(np.array([dataRowNumpyArray, ]))
-#         print(predicted)
-#         print(f'predicted class: {np.argmax(predicted[0])}')
-#         return str(np.argmax(predicted[0]))
 
-        # return predicted, np.argmax(predicted[0])
-modelName = "musicGenreModel_1000New.h5"
+# modelName = "world_music_region_model_500epocs.h5"
+modelName = "world_music_region_model_4_layers_500epochs.h5"
 model = load_model(modelName)
-model.summary()
+# model.summary()
 
 def predictRegion(audioFilePath):
-    # modelFileName = "musicGenreModel_1000New.h5"
-    y, sr = librosa.load(audioFilePath, mono=True, duration=30)
+    y, sr = librosa.load(audioFilePath, mono=True, duration=60)
     dataRow = []
     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
     dataRow.append(np.mean(chroma_stft))
@@ -84,10 +50,92 @@ def predictRegion(audioFilePath):
     for e in mfcc:
         dataRow.append(np.mean(e))
     dataRowNumpyArray = np.array(dataRow)
-    # model = load_model(modelFileName)
-    model.summary()
+    # model.summary()
     predicted = model.predict(np.array([dataRowNumpyArray, ]))
     print(predicted)
     print(f'predicted class: {np.argmax(predicted[0])}')
-    return str(np.argmax(predicted[0]))
+    return np.argmax(predicted[0])
+    # return str(np.argmax(predicted[0]))
 
+
+
+# for i in range(3):
+#     fileName = f'sample_audio/Javanese Gamelan Ensemble - Pelog Barang - Singa Nebah (The Pouncing Lion)_{i}.wav'
+#     predictRegion(fileName)
+
+# for i in range(4):
+#     fileName = f'sample_audio/Jalikunda African Drums take the Montserrat African Music Festival by storm_{i}.wav'
+#     predictRegion(fileName)
+
+# predictRegion("wm_regions/sub_sahara_africa/Angola Ngoma Dance Ensemble_00.wav")
+
+# predictions = {}
+# for i in range(7):
+#     fileName = f"sample_audio/Kpanlogo (DMC)_{i}.wav"
+#     predictedClass = predictRegion(fileName)
+#     if predictedClass in predictions:
+#         predictions[predictedClass] += 1
+#     else:
+#         predictions[predictedClass] = 1
+# print(predictions)
+
+def rankList(scores):
+    vals = {}
+    for i in range(len(scores)):
+        vals[i] = scores[i]
+    output = [None] * 4
+    sortedRanks = {k: v for k, v in sorted(vals.items(), key=lambda item: item[1], reverse=True)}
+    scoreFactor = 0
+    for key in sortedRanks:
+        output[key] = 4 - scoreFactor
+        scoreFactor += 1
+    return output
+
+def predictRegionOffset(audioFilePath, offsetValue):
+    y, sr = librosa.load(audioFilePath, offset=offsetValue, mono=True, duration=60)
+    dataRow = []
+    chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+    dataRow.append(np.mean(chroma_stft))
+    rmse = librosa.feature.rms(y=y)
+    dataRow.append(np.mean(rmse))
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    dataRow.append(np.mean(spec_cent))
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    dataRow.append(np.mean(spec_bw))
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    dataRow.append(np.mean(rolloff))
+    zcr = librosa.feature.zero_crossing_rate(y)
+    dataRow.append(np.mean(zcr))
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    for e in mfcc:
+        dataRow.append(np.mean(e))
+    dataRowNumpyArray = np.array(dataRow)
+    # model.summary()
+    predicted = model.predict(np.array([dataRowNumpyArray, ]))
+    print(predicted)
+    predictedClass = np.argmax(predicted[0])
+    print(f'predicted class: {predictedClass}')
+    # print(f'predicted class: {np.argmax(predicted[0])}')
+    return predicted[0], predictedClass
+    # return np.argmax(predicted[0])
+
+
+def predictSong(audioPath):
+    start = time.time()
+    audioLength = int(librosa.get_duration(filename=audioPath))
+    numOneMinuteChunks = audioLength // 60 #determine how many complete 1 minute chunks we need
+    classPredictions = {0:0, 1:0, 2:0, 3:0}#initialise the datastructure to keep count of predictions
+    firstPositionPredictions = {0:0, 1:0, 2:0, 3:0}
+    for i in range(numOneMinuteChunks):
+        offsetValue = i * 60
+        prediction, top = predictRegionOffset(audioFilePath=audioPath, offsetValue=offsetValue)
+        scores = rankList(prediction)
+        for i in range(len(scores)):
+            classPredictions[i] += scores[i]
+        firstPositionPredictions[top] += 1
+    sortedPredictions = {k: v for k, v in sorted(classPredictions.items(), key=lambda item: item[1], reverse=True)}
+    print(sortedPredictions)
+    print(firstPositionPredictions)
+    end = time.time()
+    print(f'predicted song in {end-start} seconds')
+    return sortedPredictions
--- new-classifier.py
+++ new-classifier.py
@@ -23,96 +23,61 @@ import keras
 import warnings
 warnings.filterwarnings('ignore')
 
-# sampleRate = 88
-# desiredDuration = 30
 
-# def predictIndividualFile(songName, model):
-#     y, sr = librosa.load(songName, mono=True, duration=30)
-#     dataRow = []
-#     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-#     dataRow.append(np.mean(chroma_stft))
-#     rmse = librosa.feature.rms(y=y)
-#     dataRow.append(np.mean(rmse))
-#     spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-#     dataRow.append(np.mean(spec_cent))
-#     spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-#     dataRow.append(np.mean(spec_bw))
-#     rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-#     dataRow.append(np.mean(rolloff))
-#     zcr = librosa.feature.zero_crossing_rate(y)
-#     dataRow.append(np.mean(zcr))
-#     mfcc = librosa.feature.mfcc(y=y, sr=sr)
-#     for e in mfcc:
-#         dataRow.append(np.mean(e))
-#     dataRowNumpyArray = np.array(dataRow)
-#     # transformedData = []
-#     # for val in dataRow:
-#     #     valRow = [val]
-#     #     transformedData.append(valRow)
-#     # print(len(dataRowNumpyArray))
-#     # print(f'transformedData: ', transformedData)
-#     # frame = pd.DataFrame()
-#     # dataRowNumpyArray.iloc[:, :-1], dtype=float)
-#     # dataRowWrapped = np.array(dataRowNumpyArray)
-#     # dataRowNumpyArray = np.array(transformedData)
-#     predicted = model.predict(np.array([dataRowNumpyArray, ]))
-#     # predicted = model.predict_classes(dataRowNumpyArray)
-#
-#     print(f'predicted class: {np.argmax(predicted[0])}')
-#     return predicted
-
-
-csvDataFileName = "outputData_new.csv"
+csvDataFileName = "world_music_data.csv"
+fileDurationInSeconds = 60
 
 #prepare csv file header
-header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
-for i in range(1, 21):
-    header += f' mfcc{i}'
-header += ' label'
-header = header.split()
-
-
-
-#extracting every audio
-
-# write header row to file
-start0 = time.time()
-file = open(csvDataFileName, 'w', newline='')
-with file:
-    writer = csv.writer(file)
-    writer.writerow(header)
+# header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+# for i in range(1, 21):
+#     header += f' mfcc{i}'
+# header += ' label'
+# header = header.split()
 #
-genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
-# def extractAudioSpecs():
-for g in genres:
-    pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)
-    for filename in os.listdir(f'genres/{g}'):
-        songname = f'genres/{g}/{filename}'
-        trackNumber = int(songname[-6:-4])
-        print(f'---reading {g} track {trackNumber}------')
-        if trackNumber < 90:
-            y, sr = librosa.load(songname, mono=True, duration=30)
-            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-            rmse = librosa.feature.rms(y=y)
-            spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-            spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-            rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-            zcr = librosa.feature.zero_crossing_rate(y)
-            mfcc = librosa.feature.mfcc(y=y, sr=sr)
-            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
-            for e in mfcc:
-                to_append += f' {np.mean(e)}'
-            to_append += f' {g}'
-            file = open(csvDataFileName, 'a', newline='')
-            with file:
-                writer = csv.writer(file)
-                writer.writerow(to_append.split())
-        else:
-            break
-    print(f'----done with {g}-------')
-
-end0 = time.time()
-print(f'----finished writing csv in {end0 - start0} seconds------')
+#
+#
+# #extracting every audio
+#
+# # write header row to file
+# start0 = time.time()
+# file = open(csvDataFileName, 'w', newline='')
+# with file:
+#     writer = csv.writer(file)
+#     writer.writerow(header)
+# #
+# # genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+#
+# wm_regions = 'latin_america_carrib south_asia south_east_asia sub_sahara_africa'.split()
+#
+# # def extractAudioSpecs():
+# for g in wm_regions:
+#     # pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)
+#     for filename in os.listdir(f'wm_regions/{g}'):
+#         songname = f'wm_regions/{g}/{filename}'
+#         # trackNumber = int(songname[-6:-4])
+#         print(f'---reading {g} track {filename}------')
+#         y, sr = librosa.load(songname, mono=True, duration=fileDurationInSeconds)
+#         chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+#         rmse = librosa.feature.rms(y=y)
+#         spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+#         spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+#         rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+#         zcr = librosa.feature.zero_crossing_rate(y)
+#         mfcc = librosa.feature.mfcc(y=y, sr=sr)
+#         filenameForCSV = filename.replace(" ", "_")
+#         to_append = f'{filenameForCSV} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+#         for e in mfcc:
+#             to_append += f' {np.mean(e)}'
+#         to_append += f' {g}'
+#         file = open(csvDataFileName, 'a', newline='')
+#         with file:
+#             writer = csv.writer(file)
+#             writer.writerow(to_append.split())
+#
+#     print(f'----done with {g}-------')
+#
+# end0 = time.time()
+# print(f'----finished writing csv in {end0 - start0} seconds------')
 
 start1 = time.time()
 data = pd.read_csv(csvDataFileName)
@@ -135,7 +100,7 @@ X = np.array(data.iloc[:, :-1], dtype=float)
 # X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
 
 #Divide into test and train
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 end1 = time.time()
 print(f'------finished reading, processing, and dividing up data in {end1-start1} seconds--------')
 
@@ -149,37 +114,23 @@ model.add(layers.Dense(128, activation='relu'))
 
 model.add(layers.Dense(64, activation='relu'))
 
-model.add(layers.Dense(10, activation='softmax'))
+model.add(layers.Dense(4, activation='softmax'))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
+
+epochs = 500
 history = model.fit(X_train,
                     y_train,
-                    epochs=10000,
+                    epochs=epochs,
                     batch_size=128)
 
 test_loss, test_acc = model.evaluate(X_test,y_test)
 print('test_acc: ',test_acc)
 end2 = time.time()
 print(f'------------finished with epochs and stuff in {end2-start2}seconds -------------')
+
 model.summary()
-modelFileName = "musicGenreModel_1000New.h5"
+modelFileName = f'world_music_region_model_4_layers_{epochs}epochs.h5'
 modelSaved = model.save(modelFileName)
-# modelSaved.summary()
-
-# testFile1 = "sample_audio/Dead Flower Complete.wav"
-# testFile2 = "sample_audio/06 Swasti Path 2 Final.mp3"
-# predictIndividualFile(testFile1, model)
-# predictIndividualFile(testFile2, model)
-
-# count = 0
-# for file in os.listdir("genres/metal"):
-#     songName = f"genres/metal/{file}"
-#     print(f"predicting {songName}-------")
-#     predictIndividualFile(songName, model)
-#     print(f"finished predicting {songName} --------------------")
-#     count += 1
-#     if count == 20:
-#         break
-# bluesSongs =
